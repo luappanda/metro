5556fe28af7e097ff8cf3bd452ec27145447b0a2
Astroverum <163025680+Astroverum@users.noreply.github.com>
2024-11-11 11:04:40 -0600

    the selection is now takes into account weighting

--- selection.py
+++ selection.py
@@ -1,13 +1,20 @@
 import os
 import geopandas as gpd
+import numpy as np
 import random
 
 #Get a GeoDataFrame of the grid
-grid_gdf = gpd.read_file("GISFiles/grid_proc.gpkg")
+grid_gdf = gpd.read_file("GISFiles/weighted grid.gpkg")
 
-viable_grids = grid_gdf[grid_gdf["CONTAINS_WATER"] == False]
+viable_grids = grid_gdf[grid_gdf["TOTAL WEIGHTED FEASIBILITY"] > 0]
+weights = viable_grids["TOTAL WEIGHTED FEASIBILITY"].values / viable_grids["TOTAL WEIGHTED FEASIBILITY"].sum()
 
-selected_grids = viable_grids.sample(n=7)
+
+num_samples = 10  # Number of points to select
+selected_indices = np.random.choice(viable_grids.index, size=num_samples, p=weights, replace=False)
+print(selected_indices)
+
+selected_grids = viable_grids.loc[selected_indices]
 
 # Create a output path for the data
 output_fp = os.getcwd() + "/GISFiles/selection.gpkg"

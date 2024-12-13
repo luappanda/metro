ce613a227c8c7987b8255f0e6e9b170e363a696a
Astroverum <163025680+Astroverum@users.noreply.github.com>
2024-12-13 07:04:25 -0600

    small changes

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print('hello')
-print("test")
+import subprocess
+
+
+subprocess.run(["python", "corridor.py"])
+
+# Run genetic-selection.py
+subprocess.run(["python", "genetic-selection.py"])
+
+subprocess.run(["python", "corridor2.py"])
+
+subprocess.run(["python", "genetic-selection-line2.py"])
+
+subprocess.run(["python", "corridor3.py"])
+
+subprocess.run(["python", "genetic-selection-line3.py"])
+
+subprocess.run(["python", "primalgo.py"])
